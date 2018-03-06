4921a81cc8d21526cb1fdc70aa5a6bcee0654f32
BillLin <bill33@LindeMacBook-Pro.local>
2018-03-06 22:37:24 +0800

    Create a RabitMQ example

---
+++ rabbitmq-spring/src/main/java/tw/bill/Producer.java
@@ -0,0 +1,16 @@
+package tw.bill;
+
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Producer {
+    public static void main(String[] args) throws InterruptedException {
+        AbstractApplicationContext ctx = new ClassPathXmlApplicationContext("beans.xml");
+        AmqpTemplate template = ctx.getBean(RabbitTemplate.class);
+        template.convertAndSend("Hello, world!");
+        Thread.sleep(1000);
+        ctx.destroy();
+    }
+}
