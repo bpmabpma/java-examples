9f161869652b01a2109e5a2ef0d8fe3bc5dcf593
BillLin <bill33@LindeMacBook-Pro.local>
2018-03-25 21:44:44 +0800

    [Multithread] Add thread pool example

---
+++ java-multithread/src/tw/bill/ThreadPoolDemo.java
@@ -0,0 +1,17 @@
+package tw.bill;
+
+import java.util.concurrent.*;
+
+public class ThreadPoolDemo {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
+//        ExecutorService service = Executors.newCachedThreadPool();
+        ExecutorService service = Executors.newFixedThreadPool(10);
+        service.execute(() -> {
+            System.out.println("execute demo");
+        });
+        Future<String> future = service.submit(() -> {
+            return "future demo";
+        });
+        System.out.println(future.get());
+    }
+}
